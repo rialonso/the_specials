f0660b96945fa75bd7b7f4f146d73c9db23c0c42
Murilo Cardoso <muri.cardoso@hotmail.com>
2023-05-07 10:28:52 -0300

    fix: refactory continue register

--- src/app/pages/continue-register/continue-register.component.ts
+++ src/app/pages/continue-register/continue-register.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserProfileService } from 'src/app/core/services/user-profile/user-profile.service';
 import { VerifyStageRegisterDataService } from 'src/app/shared/functions/verify-stage-register-data/verify-stage-register-data.service';
-import { IUserData } from 'src/app/state-management/user-data/user-data.state';
 
 @Component({
   selector: 'app-continue-register',
@@ -12,11 +10,9 @@ export class ContinueRegisterComponent implements OnInit {
 
   constructor(
     private verifyStageRegisterDataService: VerifyStageRegisterDataService,
-    private userProfileService: UserProfileService,
   ) { }
 
   ngOnInit() {
-
     this.getUserDataVerify();
   }
    getUserDataVerify() {
